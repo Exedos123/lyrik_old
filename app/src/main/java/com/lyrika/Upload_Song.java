eef0eadbb6823bc99f910a300380931af0990c1e
Exedos123 <sayhello@exedos.co>
2021-07-29 14:29:26 +0530

    commit on 29-07-21

--- app/src/main/java/com/lyrika/Upload_Song.java
+++ app/src/main/java/com/lyrika/Upload_Song.java
@@ -34,18 +34,17 @@ public class Upload_Song extends AppCompatActivity {
     public EditText songLyric;
     public String lang;
     public Button but1;
-    final String TAG="L";
+    final String TAG = "L";
     private Toolbar toolbar;
     private FrameLayout frameLayout1;
 
     public static boolean Fragment_telugu = false;
 
 
-
     private Spinner spin;
 
 
-    private ImageView  homeBtn, accountBtn;
+    private ImageView homeBtn, accountBtn;
 
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -55,17 +54,20 @@ public class Upload_Song extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload_song);
-        songTitle = (EditText) findViewById(R.id.titleSong);
-        songLyric = (EditText) findViewById(R.id.lyricSong);
-        
-        but1 = (Button) findViewById(R.id.uplSong);
+
 
         frameLayout1 = findViewById(R.id.Song_Lyric_fragment_Container);
-        if (Fragment_telugu) {
-            Fragment_telugu = false;
-            setDefaultFragment(new Telugu_Lyrik());
+        setDefaultFragment(new Telugu_Lyrik());
 
-        }
+      /*
+        songTitle = (EditText) findViewById(R.id.titleSong);
+        songLyric = (EditText) findViewById(R.id.lyricSong);
+
+        but1 = (Button) findViewById(R.id.uplSong);
+
+
+
+
 
         toolbar = findViewById(R.id.myToolBar);
         setSupportActionBar(toolbar);
@@ -74,12 +76,11 @@ public class Upload_Song extends AppCompatActivity {
         toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.myToolBar);
 
 
-
-
-
         homeBtn = findViewById(R.id.Home_btn);
 
         accountBtn = findViewById(R.id.account_img);
+
+
 
 
         homeBtn.setOnClickListener(new View.OnClickListener() {
@@ -98,31 +99,31 @@ public class Upload_Song extends AppCompatActivity {
 
         Spinner spin = (Spinner) findViewById(R.id.Spinner);
 
-        ArrayAdapter<String> myAdapter= new ArrayAdapter<String>(Upload_Song.this,android.R.layout.simple_list_item_1,
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Upload_Song.this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.Select_language));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(myAdapter);
 
 
-        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view,
                                        int position, long id) {
 
                 //Toast.makeText(Upload_Song.this, spin.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
-                if (spin.getSelectedItem().equals("Telugu") ){
-                    lang= "Lyrik_T";
+                if (spin.getSelectedItem().equals("Telugu")) {
+                    lang = "Lyrik_T";
                     Toast.makeText(Upload_Song.this, "u r selected telugu", Toast.LENGTH_SHORT).show();
                 }
-                if (spin.getSelectedItem().equals("English") ){
-                    lang= "Lyrik_E";
+                if (spin.getSelectedItem().equals("English")) {
+                    lang = "Lyrik_E";
                     Toast.makeText(Upload_Song.this, "u r selected English", Toast.LENGTH_SHORT).show();
                 }
-                if (spin.getSelectedItem().equals("Hindi") ){
-                    lang= "Lyrik_H";
+                if (spin.getSelectedItem().equals("Hindi")) {
+                    lang = "Lyrik_H";
                     Toast.makeText(Upload_Song.this, "u r selected Hindi", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     Toast.makeText(Upload_Song.this, "please select language", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -148,18 +149,14 @@ public class Upload_Song extends AppCompatActivity {
         //Appbar Code
 
 
-
-
-
-
         but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String TiSong = songTitle.getText().toString();
                 String LySongT = songLyric.getText().toString();
-                String LySongE= songLyric.getText().toString();
-                String LySongH= songLyric.getText().toString();
-                uploadSong(TiSong,LySongT,LySongE,LySongH);
+                String LySongE = songLyric.getText().toString();
+                String LySongH = songLyric.getText().toString();
+                uploadSong(TiSong, LySongT, LySongE, LySongH);
                 Toast.makeText(Upload_Song.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
 
             }
@@ -168,15 +165,10 @@ public class Upload_Song extends AppCompatActivity {
         //Spinner
 
 
-
         ///Spinner
 
-
-
-
-
+*/
     }
-
 
 
 //////////////////user upload songs
@@ -211,50 +203,43 @@ public class Upload_Song extends AppCompatActivity {
     //////////////////user upload songs
 
 
-
-
-
-
-protected void uploadSong(String tS, String tL, String lySongE, String lySongH) {
-    // Create a new user with a first, middle, and last name
-    Map<String, Object> user = new HashMap<>();
-    String TiSong = songTitle.getText().toString();
-    String tS1 = tS;
-    String tL1 = tL;
-   // String tL2 = lySongE;
-  //  String tL3 = lySongH;
-    user.put("Title", tS1);
-    user.put(lang, tL1);
- //   user.put("Lyrik_E", tL2);
-   // user.put("Lyrik_H", tL3);
-
-
+    protected void uploadSong(String tS, String tL, String lySongE, String lySongH) {
+        // Create a new user with a first, middle, and last name
+        Map<String, Object> user = new HashMap<>();
+        String TiSong = songTitle.getText().toString();
+        String tS1 = tS;
+        String tL1 = tL;
+        // String tL2 = lySongE;
+        //  String tL3 = lySongH;
+        user.put("Title", tS1);
+        user.put(lang, tL1);
+        //   user.put("Lyrik_E", tL2);
+        // user.put("Lyrik_H", tL3);
 
 
 // Add a new document with a generated ID
 
-    db.collection("Songs")
-            .document(TiSong)
-            .set(user)
-            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
+        db.collection("Songs")
+                .document(TiSong)
+                .set(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
 
-                    if (task.isSuccessful()) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + TiSong);
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "DocumentSnapshot added with ID: " + TiSong);
 
 
-                        Intent intent1 = new Intent(Upload_Song.this, MainActivity.class);
+                            Intent intent1 = new Intent(Upload_Song.this, MainActivity.class);
 
-                        startActivity(intent1);
+                            startActivity(intent1);
+                        } else {
+                            String error = task.getException().getMessage();
+                            Toast.makeText(Upload_Song.this, error, Toast.LENGTH_SHORT).show();
+
+                        }
                     }
-                    else {
-                        String error = task.getException().getMessage();
-                        Toast.makeText(Upload_Song.this, error, Toast.LENGTH_SHORT).show();
-
-                    }
-                }
-            });
+                });
     }
 
 
@@ -282,7 +267,7 @@ protected void uploadSong(String tS, String tL, String lySongE, String lySongH) 
 
     private void setDefaultFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(frameLayout1.getId(),fragment);
+        fragmentTransaction.replace(frameLayout1.getId(), fragment);
         fragmentTransaction.commit();
     }
 
